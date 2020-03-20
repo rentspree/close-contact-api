1bbd27b2b249d0891b4cc54ceb1d88d29aef2655
Jirayu Roungsuriyaviboon <odlho4869@gmail.com>
2020-03-20 13:01:58 +0700

    add error utilities

---
+++ src/constants/errors.js
@@ -0,0 +1,9 @@
+export const ERROR_NAME = {
+  NOT_FOUND_ERROR: "NotFoundError",
+  UNAUTHORIZED_ERROR: "UnAuthorizedError",
+  TOKEN_EXPIRED: "TokenExpired",
+}
+
+export const ERROR_CODE = {
+  INCORRECT_CREDENTIALS: "incorrect_credentials",
+}

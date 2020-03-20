b54d2d42ea301959338f13e2d1eb5ae1014f052e
Suppanat Hussarangsri <suppanat@rentspree.com>
2020-03-20 14:49:39 +0700

    add random close contact seed

--- seed/seeder.js
+++ seed/seeder.js
@@ -1,17 +1,38 @@
 /* eslint-disable no-console */
+import moment from "moment"
 import userSeeder from "./user-seeder"
 import closeContactSeeder from "./close-contact-seeder"
 import dropDB from "./drop-db"
+
+function randomBetween(number) {
+  return Math.floor(Math.random() * number)
+}
 
 async function seed() {
   // seed sample
   console.log("Drop DB")
   await dropDB()
 
-  console.log("Start seed")
+  console.log("Seed users")
   const users = await userSeeder()
-  const closeContact = await closeContactSeeder(users[0], users[1])
-  console.log(closeContact)
+
+  console.log("Seed random closeContact from users")
+  const closeContactSeederList = []
+  for (let i = 0; i < 40; i += 1) {
+    const contact = randomBetween(users.length)
+    const contactee = randomBetween(users.length)
+    const days = randomBetween(30)
+    if (contact !== contactee)
+      closeContactSeederList.push(
+        closeContactSeeder(
+          users[contact],
+          users[contactee],
+          moment().subtract(days, "days"),
+        ),
+      )
+  }
+  await Promise.all(closeContactSeederList)
+
   console.log("Seed success")
   process.exit(0)
 }

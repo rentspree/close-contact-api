f73dfaabd789a4f661c692a954badd1b6e777450
greenrenge <sorasak@rentspree.com>
2020-03-20 15:36:52 +0700

    find findAll create

--- src/models/schema.js
+++ src/models/schema.js
@@ -1,19 +1,23 @@
 import { schemaComposer } from "graphql-compose"
-import { UserTC, User } from "./user"
+import { UserTC } from "./user"
 // Requests which read data put into Query
 
+// schemaComposer.Query.addFields({
+//   users: {
+//     type: "[User]",
+//     resolve: async () => {
+//       const users = await User.find()
+//       return users
+//     },
+//   },
+// })
+
 schemaComposer.Query.addFields({
-  users: {
-    type: "[User]",
-    resolve: async () => {
-      const users = await User.find()
-      return users
-    },
-  },
+  users: UserTC.getResolver("findMany"),
 })
 
 schemaComposer.Query.addFields({
-  user: UserTC.getResolver("findMany"),
+  user: UserTC.getResolver("findOne"),
 })
 
 schemaComposer.Mutation.addFields({

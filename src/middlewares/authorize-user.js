885cb36b2913e20788c959ae4ae1189f34c7eb0c
Jirayu Roungsuriyaviboon <odlho4869@gmail.com>
2020-03-20 17:00:28 +0700

    add refresh token api and authorization middleware

---
+++ src/middlewares/authorize-user.js
@@ -0,0 +1,73 @@
+import passport from "passport"
+import { Strategy as BearerStrategy } from "passport-http-bearer"
+import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt"
+import fs from "fs"
+import path from "path"
+import { User } from "../models/user"
+
+export const authorizeIgnoreExpires = () => [
+  passport.authenticate("jwtIgnoreExpires", { session: false }),
+]
+
+passport.use(
+  "accessToken",
+  new BearerStrategy((accessToken, done) => done(null, true)),
+)
+
+passport.use(
+  "jwtIgnoreExpires",
+  new JwtStrategy(
+    {
+      ignoreExpiration: true,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("Bearer"),
+      secretOrKey: fs.readFileSync(
+        path.resolve(__dirname, "../../certs", "public.pem"),
+      ),
+    },
+    async (jwtPayload, done) => {
+      try {
+        if (jwtPayload) {
+          done(null, jwtPayload)
+        } else {
+          done(null, false)
+        }
+      } catch (e) {
+        done(e, false)
+      }
+    },
+  ),
+)
+
+passport.use(
+  "jwt",
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("Bearer"),
+      secretOrKey: fs.readFileSync(
+        path.resolve(__dirname, "../../certs", "public.pem"),
+      ),
+    },
+    async (jwtPayload, done) => {
+      try {
+        const user = await User.findById(jwtPayload._id)
+        if (user) {
+          done(null, user)
+        } else {
+          done(null, false)
+        }
+      } catch (e) {
+        done(e, false)
+      }
+    },
+  ),
+)
+
+export const authorize = () => [
+  passport.authenticate("jwt", { session: false }),
+]
+
+export function stripToken(req) {
+  return req.headers.authorization
+    ? req.headers.authorization.replace(/^Bearer\s/, "")
+    : null
+}
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,11 @@
 import express from "express"
-import LoginHandler from "../middlewares"
+import LoginHandler from "../middlewares/login"
+import { authorizeIgnoreExpires } from "../middlewares/authorize-user"
+import refreshTokenHandler from "../middlewares/refresh-token"
 
 const router = express.Router()
 
 router.use(LoginHandler)
+router.use(authorizeIgnoreExpires(), refreshTokenHandler)
 
 module.exports = router

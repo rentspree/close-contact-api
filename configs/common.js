fca9a6e14a370db7f946bd8007881faf77eccdfc
Ton Vudhidej <vudhidej@rentspree.com>
2020-03-31 14:34:46 +0700

    integrate makeNotification with FCM

--- configs/common.js
+++ configs/common.js
@@ -23,4 +23,9 @@ module.exports = {
   imagePath: process.env.IMAGE_PATH || path.resolve(__dirname, "../image_tmp"),
   "@fbAppSecret:FB_APP_SECRET": "372eadb5d30767f287c1746ad55a4748",
   "@graphiql:IS_DEV": "true",
+  fcm: {
+    "@endpoint:FCM_ENDPOINT": "https://fcm.googleapis.com/fcm/send",
+    "@serverKey:FCM_SERVER_KEY":
+      "AAAAnaRVg2o:APA91bHc6sr-mTgKejgzahwtXKw3pQVEEvbtelKLs6UFEvrVOEN8lRP8iz-s6Mof6avzwjNaIZIgY4mPpn0Ie6f0QdiWFgNsxEyc_r4hX5x9cNT7ceTVBFLVMrkRvBikio5Jg2qWQnyK",
+  },
 }
--- src/service/notification-service.js
+++ src/service/notification-service.js
@@ -2,35 +2,36 @@
 /* eslint-disable no-unused-vars */
 import moment from "moment"
 import { Types } from "mongoose"
+import axios from "axios"
 import { CloseContact } from "../models/close-contact"
 import { User, STATUS_ENUM } from "../models/user"
 import { Notification } from "../models/notification"
+import { DeviceToken, TYPE_ENUM } from "../models/device-token"
+import config from "../config"
 
 const { ObjectId } = Types
 
-const config = {
-  [STATUS_ENUM.INFECTED]: (actorId, location, timestamps) => ({
-    title: "INFECTION ALERT",
-    description: `User ${actorId} marked to be infected , you meet each other at location ${location} on ${moment(
-      timestamps,
-    ).toString()}`, // TODO: format msg
-    type: "infection_alert", // TODO: make an enum type
+const notificationConfig = {
+  [STATUS_ENUM.INFECTED]: actorName => ({
+    title: actorName,
+    description: "อัปเดตสถานะเป็น มีเชื้อ", // TODO: format msg
+    type: STATUS_ENUM.INFECTED, // TODO: make an enum type
   }),
-  [STATUS_ENUM.HEALED]: (actorId, location, timestamps) => ({
-    title: "HEALTHY ALERT",
-    description: `User ${actorId} marked healthy , you meet each other at location ${location} on ${moment(
-      timestamps,
-    ).toString()}`, // TODO: format msg
-    type: "healthy_alert", // TODO: make an enum type
+  [STATUS_ENUM.HEALED]: actorName => ({
+    title: actorName,
+    description: "อัปเดตสถานะเป็น หายแล้้ว", // TODO: format msg
+    type: STATUS_ENUM.HEALED, // TODO: make an enum type
   }),
 }
 
-export const makeNotification = (actorId, status, notiTimeStamp) => {
+export const makeNotification = async (actorId, status, notiTimeStamp) => {
   // make sure noti is made in FIFO thing. // so why timestamp is used here
+  const { firstName, lastName } = await User.findById(actorId)
+  const actorName = [firstName, lastName].join(" ")
   const sentUser = {}
   CloseContact.getContactForNotify(actorId)
     .cursor()
-    .on("data", closeContact => {
+    .on("data", async closeContact => {
       const { contact, contactee, timestamps, location } = closeContact
       const userToNotify = contact === actorId ? contactee : contact
 
@@ -38,15 +39,43 @@ export const makeNotification = (actorId, status, notiTimeStamp) => {
       if (!sentUser[`${userToNotify}`]) {
         sentUser[`${userToNotify}`] = true // mark for distinct
         // TODO: query user's device to make notification
-
+        const deviceTokens = DeviceToken.find({ user: userToNotify }) || {}
         const noti = new Notification({
           // REVIEW select the first one or the last one ?
           notifier: ObjectId(userToNotify),
           actor: ObjectId(actorId),
           timestamps: moment(notiTimeStamp),
-          ...config[status](actorId, location, timestamps),
+          ...notificationConfig[status](actorName),
           // read: ,
         })
+        Promise.all(
+          deviceTokens.map(({ token, type }) => {
+            if (type === TYPE_ENUM.FCM) {
+              return axios.post(
+                config.fcm.endpoint,
+                {
+                  to: token,
+                  data: {
+                    title: noti.title,
+                    body: noti.description,
+                  },
+                },
+                {
+                  headers: {
+                    "Content-Type": "application/json",
+                    Host: "fcm.googleapis.com",
+                    Authorization: `key=${config.fcm.serverKey}`,
+                  },
+                },
+              )
+            }
+            if (type === TYPE_ENUM.APNS) {
+              // TODO: integrate with APNS
+              return null
+            }
+            return null
+          }),
+        )
         noti.save((err, doc) => {
           if (err) {
             console.error("save noti error", err)
